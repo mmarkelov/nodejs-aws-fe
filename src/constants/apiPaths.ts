aa4611fb08341fc222c52c736286b971488497de
Maksim Markelov <maks-markel@mail.ru>
2020-11-19 16:52:49 +0300

    Add ability to upload file (#4)
    * Add ability to upload file
    
    * Fix ProductsTable response

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://ox1m9ngpvi.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://ox1m9ngpvi.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://ox1m9ngpvi.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://o1qomq4422.execute-api.eu-west-1.amazonaws.com/dev/',
   bff: 'https://ox1m9ngpvi.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
