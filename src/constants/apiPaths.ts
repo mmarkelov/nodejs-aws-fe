245cd74c6370c8ebe9026ee52fcf847e8f6430e2
mmarkelov <maks-markel@mail.ru>
2020-11-19 20:12:08 +0300

    Fix paths

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,8 +1,8 @@
 
 const API_PATHS = {
-  product: 'https://ox1m9ngpvi.execute-api.eu-west-1.amazonaws.com/dev',
+  product: 'https://t5fbnxql2m.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://ox1m9ngpvi.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://o1qomq4422.execute-api.eu-west-1.amazonaws.com/dev/',
+  import: 'https://ea0hxt57b7.execute-api.eu-west-1.amazonaws.com/dev/',
   bff: 'https://ox1m9ngpvi.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
