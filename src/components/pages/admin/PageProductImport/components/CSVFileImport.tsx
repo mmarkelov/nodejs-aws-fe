52a221503aa83e721f97c6f2adad0d14b2a0e57b
mmarkelov <maks-markel@mail.ru>
2020-11-27 12:23:11 +0300

    Pass auth for importing

--- src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
+++ src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
@@ -32,10 +32,14 @@ export default function CSVFileImport({url, title}: CSVFileImportProps) {
 
   const uploadFile = async () => {
       const authToken = localStorage.getItem('authorization_token');
+      console.log(authToken)
       // Get the presigned URL
       const response = await axios({
         method: 'GET',
-        url: `${url}?name=${encodeURIComponent(file.name)}`,
+        url,
+        params: {
+          name: encodeURIComponent(file.name),
+        },
         headers: {
            Authorization: `Basic ${authToken}`
         }
