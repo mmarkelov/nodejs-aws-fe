6cfb01cf5149424ea8a03c9fb9552f02103693ce
mmarkelov <maks-markel@mail.ru>
2020-12-05 17:05:15 +0300

    Rollback import API

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://t5fbnxql2m.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://t5fbnxql2m.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://ea0hxt57b7.execute-api.eu-west-1.amazonaws.com/dev/',
+  import: 'https://ofx9rhrib9.execute-api.eu-west-1.amazonaws.com/dev/',
   bff: 'https://t5fbnxql2m.execute-api.eu-west-1.amazonaws.com/dev',
   cart: 'http://mmarkelov-cart-api.eu-west-1.elasticbeanstalk.com/api',
 };
