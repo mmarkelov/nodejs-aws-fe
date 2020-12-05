d9d8728cecfceae2f487f29d2a1692037be303e2
mmarkelov <maks-markel@mail.ru>
2020-12-05 16:48:21 +0300

    Card API

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
   order: 'https://t5fbnxql2m.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://ea0hxt57b7.execute-api.eu-west-1.amazonaws.com/dev/',
   bff: 'https://t5fbnxql2m.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'http://mmarkelov-cart-api.eu-west-1.elasticbeanstalk.com/api',
 };
 
 export default API_PATHS;
