daf49c5489f2479e054e465a2165b1116b4a1ad0
Maksim Markelov <maks-markel@mail.ru>
2020-12-13 15:06:59 +0300

    Cart api (#7)
    * feat: Add npm commands
    
    * fix: add random for product images
    
    * Update logic for uploading products files
    
    * Add cart api integration
    
    * Card API
    
    * Rollback import API
    
    * Fix Card API
    
    Co-authored-by: SergeyKovalchuk <sergeyalexkoval@gmail.com>
    Co-authored-by: Konstantin Shavrin <Konstantin_Shavrin@epam.com>
    Co-authored-by: Borys Semerenko <borys_semerenko@epam.com>
    Co-authored-by: Herman_Bilous <Herman_Bilous@epam.com>

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
   order: 'https://t5fbnxql2m.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://ofx9rhrib9.execute-api.eu-west-1.amazonaws.com/dev/',
   bff: 'https://t5fbnxql2m.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'http://mmarkelov-cart-api.eu-west-1.elasticbeanstalk.com/api',
+  cart: 'http://mmarkelov-cart-api-dev.eu-west-1.elasticbeanstalk.com/api',
 };
 
 export default API_PATHS;
